b4aaa1396f7a692a42e1f5641988d6c3668ff922
mikey <mforest@protonmail.com>
2023-02-26 11:10:56 -0500

    updates

--- _nuxt/Markdown.77f6fc27.js
+++
@@ -1,1 +0,0 @@
-import s from"./ContentSlot.1541d842.js";import{W as o,av as u,S as f,A as m}from"./query.9163927d.js";import"./entry.3931d2f6.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),r=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
---
+++ _nuxt/ProseEm.2555d76a.js
@@ -0,0 +1,1 @@
+import{a as r}from"./entry.8330a62a.js";import{af as o,ag as a,aC as t}from"./query.9163927d.js";const s={};function n(e,c){return o(),a("em",null,[t(e.$slots,"default")])}const m=r(s,[["render",n]]);export{m as default};
