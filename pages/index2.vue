607c765deaddcd6f056dbcf0237253ad166eaca6
mikey <mforest@protonmail.com>
2023-03-08 04:29:52 -0500

    Deploying static site

--- pages/index2.vue
+++ pages/index2.vue
@@ -1,37 +1,34 @@
 <template>
     <div class="flex items-center justify-center text-white h-screen max-w-6xl mx-auto">
-
-            <div class="p-10">
-              <h1 id="heading" class="pb-10 tracking-wide text-8xl font-display xs:text-3xl sm:text-4xl md:text-6xl lg:text-8xl max-h-screen   text-stone-100 select-none">
-              <span class="letter">Front</span>
-              <span class="letter">-</span>
-              <span class="letter">End </span>
-              <span class="letter">Developer </span>
-              <span class="letter">& </span>
-              <span class="letter">Project </span>
-              <span class="letter">Manager</span>
-              <span class="letter">.</span>
-              </h1>  
-              <h1 class=" text-white text-lg ml-auto w-full inline-block select-none" >
-                <nuxt-link to="/about"  id="link1">
-                  <span class="transition ease-in-out border-2 border-white p-2 px-3 md:p-3 md:px-4 text-base md:text-lg mr-4 hover:bg-white hover:!text-[#131721] hover:transition-all">Learn More</span>
-                </nuxt-link>
-                <a href="https://airtable.com/shrTD2l45wB1qa6Pb" class="group block md:inline-block mt-6 md:mt-0" id="link2">Let's Talk
-                  <span class="group-hover:pl-2 group-hover:transition-all text-bluegreen group-hover:text-bluegreen-light">&#8594;</span>
-                </a>
-              </h1>
-            </div>
-            <div class="hidden absolute bottom-0">
-              <a rel="me" href="https://mstdn.social/@letsbecomehuman">Mastodon Verification</a>
-
-      </div> 
+        <div class="p-10">
+            <h1 id="heading" class="pb-10 tracking-wide text-8xl font-display xs:text-3xl sm:text-4xl md:text-6xl lg:text-8xl max-h-screen   text-stone-100 select-none">
+            <span class="letter">Front</span>
+            <span class="letter">-</span>
+            <span class="letter">End </span>
+            <span class="letter">Developer </span>
+            <span class="letter">& </span>
+            <span class="letter">Project </span>
+            <span class="letter">Manager</span>
+            <span class="letter">.</span>
+            </h1>  
+            <h1 class=" text-white text-lg ml-auto w-full inline-block select-none" >
+            <nuxt-link to="/about"  id="link1">
+                <span class="transition ease-in-out border-2 border-white p-2 px-3 md:p-3 md:px-4 text-base md:text-lg mr-4 hover:bg-white hover:!text-[#131721] hover:transition-all">Learn More</span>
+            </nuxt-link>
+            <a href="https://airtable.com/shrTD2l45wB1qa6Pb" class="group block md:inline-block mt-6 md:mt-0" id="link2">Let's Talk
+                <span class="group-hover:pl-2 group-hover:transition-all text-bluegreen group-hover:text-bluegreen-light">&#8594;</span>
+            </a>
+            </h1>
+        </div>
+        <div class="hidden absolute bottom-0">
+            <a rel="me" href="https://mstdn.social/@letsbecomehuman">Mastodon Verification</a>
+        </div> 
     </div>
   </template>
   
   <script setup>
     import gsap from 'gsap';
     import { onMounted } from "vue";
-  
   
     definePageMeta({    layout: 'default'  })
     const runtimeConfig = useRuntimeConfig()
@@ -72,7 +69,6 @@
       delay: 1.3
    });
   })
-  
   </script>
   
   <style scoped>
